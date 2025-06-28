3d039ac2bc63fc1d0746ce1acb99a1fe1a2b507b
Vraj Parikh <vrajaparikh01@gmail.com>
2025-06-28 23:56:26 +0530

    initial commit
    Signed-off-by: Vraj Parikh <vrajaparikh01@gmail.com>

--- src/Counter.sol
+++ src/Counter.sol
@@ -2,13 +2,5 @@
 pragma solidity ^0.8.13;
 
 contract Counter {
-    uint256 public number;
 
-    function setNumber(uint256 newNumber) public {
-        number = newNumber;
-    }
-
-    function increment() public {
-        number++;
-    }
 }
